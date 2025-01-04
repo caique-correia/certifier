6fa77884ddc6d995bf249982aab75483fc641aea
caique-correia <caiquecorreia.gama@hotmail.com>
2025-01-04 09:05:05 -0300

    feat: establish header format;

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
-    return(
-        <header>
-            HEADER
+    return (
+        <header className="w-full bg-blue-600 text-white flex justify-evenly items-center p-4">
+            <div>CERTIFICADORA</div>
+
+            <div>FALE CONOSCO</div>
+
+            <div>LOG IN</div>
         </header>
     );
 }
